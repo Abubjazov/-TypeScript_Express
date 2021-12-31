7ea08dfdfdd6c1774d79b069b349540803e31212
Radik Abubjazov <abubjazov@mail.ru>
2022-01-01 00:43:24 +0300

    SRV: add Express

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -1,1 +1,16 @@
-console.log(`Hello Radeg!!! ) It's ok`)
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div>
+            <h1> Hello! ) </h1>        
+        </div>
+        `
+    )
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port: 3000')
+})
