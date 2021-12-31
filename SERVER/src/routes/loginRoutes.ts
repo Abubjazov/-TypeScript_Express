71b7244fa0d36e299737258c1b947970d7429cf0
Radik Abubjazov <abubjazov@mail.ru>
2022-01-01 01:40:20 +0300

    SRV: setup Router

--- SERVER/src/routes/loginRoutes.ts
+++ SERVER/src/routes/loginRoutes.ts
@@ -2,13 +2,31 @@ import { Request, Response, Router } from 'express';
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
     res.send(`
         <div>
-            <h1> Hello!!! ) </h1>        
+            <h1>Welcome to our website</h1>
+            <hr>
+            <form method="POST">
+                <div>
+                    <label>Your Email</label>
+                    <input name="email" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input name="password" type="password" />
+                </div>
+                <button type="submit">Log in</button>
+            </form>       
         </div>
         `
     )
+})
+
+router.post('/login', (req: Request, res: Response) => {
+    const {email, password} = req.body
+
+    res.send(email + " " + password)
 })
 
 export { router }
