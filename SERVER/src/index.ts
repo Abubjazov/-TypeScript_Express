2f8e985cee76838b8701938c0944d1ecd47cf7d9
Radik Abubjazov <abubjazov@mail.ru>
2022-01-01 23:21:58 +0300

    SRV: create class Server

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -2,12 +2,30 @@ import cookieSession from 'cookie-session'
 import express from 'express'
 import { router } from './routes/loginRoutes'
 
-const app = express()
+// const app = express()
 
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieSession({ keys: ['dghd53554354tgdfdgf'] }))
-app.use(router)
+// app.use(express.urlencoded({ extended: true }))
+// app.use(cookieSession({ keys: ['dghd53554354tgdfdgf'] }))
+// app.use(router)
 
-app.listen(3000, () => {
-    console.log('Server started on port: 3000')
-})
+// app.listen(3000, () => {
+//     console.log('Server started on port: 3000')
+// })
+
+class Server {
+    app: express.Express = express()
+
+    constructor() {
+        this.app.use(express.urlencoded({ extended: true }))
+        this.app.use(cookieSession({ keys: ['dghd53554354tgdfdgf'] }))
+        this.app.use(router)
+    }
+
+    start(): void {
+        this.app.listen(3000, () => {
+            console.log('Server started on port: 3000')
+        })
+    }
+}
+
+new Server().start()
