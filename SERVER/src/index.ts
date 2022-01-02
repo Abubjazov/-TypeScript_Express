5f056122f047d0a7d6704e14f9a97110defd9571
Radik Abubjazov <abubjazov@mail.ru>
2022-01-03 02:02:02 +0300

    SRV: refactor

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -2,14 +2,13 @@ import cookieSession from 'cookie-session'
 import express from 'express'
 import { AppRouter } from './AppRouter'
 import './controllers/LoginController'
-import { router } from './routes/loginRoutes'
+import './controllers/RootController'
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieSession({ keys: ['dghd53554354tgdfdgf'] }))
 app.use(AppRouter.getInstance())
-app.use(router)
 
 app.listen(3000, () => {
     console.log('Server started on port: 3000')
